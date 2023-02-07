455866f2c08ccc1258bb07e6f9d41f64c98e154e
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-02-08 00:16:06 +0700

    ho

--- resources/views/admin/transaksi/booking.blade.php
+++ resources/views/admin/transaksi/booking.blade.php
@@ -7,12 +7,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 id="token" class="m-0 text-dark">Wellcome {{ Auth::user()->name }}</h1>
+                <h1 id="token" class="m-0 text-dark">DAFTAR TRANSAKSI BOOKING</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Dashboard v1</li>
+                    <li class="breadcrumb-item active">Booking</li>
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
